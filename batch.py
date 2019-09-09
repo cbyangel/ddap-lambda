7113020a6014747f1a3b563a9e70b86386676ecd
elon <choi.wi@gsshop.com>
2019-09-09 17:38:37 +0900

    fix batch import clause

--- batch.py
+++ batch.py
@@ -1,12 +1,11 @@
 #!/opt/anaconda3/envs/ddap/bin/python
 
 import pandas as pd
-import src.dbutil
-import src.write_db
+from src import write_db
 from main import main
 
 if __name__ == '__main__':
     broad_dt = (pd.Timestamp.now() - pd.Timedelta('1 d')).strftime('%Y%m%d')
-    src.write_db.delete_by_day_auto_table(broad_dt)
-    src.write_db.delete_by_day_snapshot_table(broad_dt)
+    write_db.delete_by_day_auto_table(broad_dt)
+    write_db.delete_by_day_snapshot_table(broad_dt)
     main(broad_dt)
